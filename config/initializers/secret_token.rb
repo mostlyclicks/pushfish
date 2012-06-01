c1db93a40b0ab5ec79d45015b0c28ec4bf62ba8a
mostlyclicks <carlos@mostlyclicks.com>
2012-06-01 09:36:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pushfish::Application.config.secret_token = '8153c6792500d0d1f546fc735837f9b9bcf0da28526347e898e1b9917a515e079c3da26c52478a234b9fb729ededca54aa1e4c589326a8254d9c58a2a59883cd'
